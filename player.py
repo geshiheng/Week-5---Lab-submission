bbf54e38b126f28617e35a77e1e44dc4cdc6802f
geshiheng <ge952893553@gmail.com>
2022-11-14 14:33:13 -0800

    modified:   player.py 	modified:   test.py

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-import math
+import random
 
 
 class Player():
@@ -28,5 +28,10 @@ class HumanPlayer(Player):
         return val
 
 
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
 
-
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 
 from logic import TicTacToe
 from logic import play
-from player import HumanPlayer
-
+from player import HumanPlayer, RandomComputerPlayer
 
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
+    x_player = RandomComputerPlayer('X')
     o_player = HumanPlayer('O')
     t = TicTacToe()
     play(t, x_player, o_player, print_game=True)
